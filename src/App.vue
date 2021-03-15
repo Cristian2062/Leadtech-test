3e66de7e0bc28b922f409cd77a118bac356e3c8d
Cristian2062 <baner2062@gmail.com>
2021-03-15 09:38:52 +0100

    vol.1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    
+    <Slider />
 
   </div>
 </template>
 
 <script>
-
+import Slider from './components/Slider'
 
 export default {
   name: 'App',
   components: {
-    
+    Slider
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:white;
 }
 </style>
